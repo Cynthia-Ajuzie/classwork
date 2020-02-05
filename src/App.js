27094a66dad968cc442ce758c967f2dd14100d2b
cindy_nonye <ajuziecynthia@gmail.com>
2020-02-05 15:29:31 +0000

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+     toggle: true 
+  }
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      <Welcome text="welcome to using props" toggle={this.state.toggle}
+              //  props1="you can trust my services"
+              //  props2="kudos"
+            /> 
+      </header> 
+        
+      <p className="App-intro">
+        To get started, Edit <code>src/App.js</code> and save to reload.
+      </p>
+    {this.state.toggle &&
+      <p>This should show and hide</p>
+    }
+      <button onClick={this.toggle}>show/hide</button>
+
     </div>
   );
+  }
+}
+ 
+class Welcome extends Component{
+  render() {
+    const{ text, toggle } = this.props;
+    console.log(toggle);
+    return(
+      <div>
+        <h2 className="App-title">{text}</h2>
+        <h2>CYNTHIA</h2>
+        <MyFunction/>
+        </div>
+    )
+
+  }
+
 }
 
+
 export default App;
+
+const MyFunction = () => <p>Ajuzie</p>
+
+
+// const myFunction = () => {
+//  return(
+//   <p>Chidera</p>
+//  )
+// }
+// explicit return which involves return
+
+// const myFunction2 = () => (
+//   <p>Chidera</p>
+// )
+
+// const myFunction3 =() => <p>Chidera</p>
+
+// both myFunction2&3 are implicit returns
